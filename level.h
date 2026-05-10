811871ad3b730400392cf28fd490aa2dd6aa411f
William de Souza Bezerra <wsb@cesar.school>
2026-05-10 12:28:05 -0300

    Dados de desenvolvimento do mapa

---
+++ level.h
@@ -0,0 +1,29 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <stdbool.h>
+
+#define LARGURA_MAPA  13
+#define ALTURA_MAPA   11
+
+typedef enum {
+    TILE_VAZIO,
+    TILE_PAREDE,
+    TILE_BLOCO,
+    TILE_PORTA
+} TipeTile;
+
+typedef struct {
+    TipeTile grade[ALTURA_MAPA][LARGURA_MAPA];
+    int tamanho_tile;
+    int tamanho_tile_h;
+} Level;
+
+void     level_iniciar(Level *level);
+void     level_atualizar_tile(Level *level);
+void     level_desenhar(Level *level);
+bool     level_pode_mover(Level *level, int tile_x, int tile_y);
+void     level_abrir_porta(Level *level);
+TipeTile level_get_tile(Level *level, float px, float py);
+
+#endif
