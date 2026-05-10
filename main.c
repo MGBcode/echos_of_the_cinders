602a3ec21bd94bbb27c44eab7a7c98ff61c853f7
William de Souza Bezerra <wsb@cesar.school>
2026-05-10 12:29:28 -0300

    Implentação do mapa na main
    Added level initialization and update functionality.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "player.h"
+#include "level.h"
 
 int main(void) {
     const int screenWidth = 800;
@@ -8,24 +9,26 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Echos of the Cinders - Jogo Modular");
 
     Player cavaleiro;
-    
-    InitPlayer(&cavaleiro, screenWidth, screenHeight); 
+    InitPlayer(&cavaleiro, screenWidth, screenHeight);
+
+    Level level;
+    level_iniciar(&level);
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime(); 
+        float deltaTime = GetFrameTime();
 
-        UpdatePlayer(&cavaleiro, deltaTime);
+        level_atualizar_tile(&level);
+
+        UpdatePlayer(&cavaleiro, deltaTime, &level);
 
         BeginDrawing();
             ClearBackground(BLACK);
-
+            level_desenhar(&level);
             DrawPlayer(cavaleiro);
-            
             DrawText("Aperte ESPACO para Dash", 20, 20, 20, LIGHTGRAY);
             DrawText(TextFormat("Cooldown: %.2f", cavaleiro.cooldownTimeCounter), 20, 50, 20, RED);
-
         EndDrawing();
     }
 
